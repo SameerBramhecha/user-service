796dee451649c8945078191f4cbf0f6b1e6a6c7b
Sameer Manoj Bramhecha <100222138+SameerBramhecha@users.noreply.github.com>
2024-10-17 09:56:59 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, loginUser, hello } = require('../controllers/userController');
+const { registerUser, loginUser, hello, getAllUsers } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/',hello);
+router.get('/users', getAllUsers);
 
 module.exports = router;
